8bf810bcabedadd1fa547749ee3fa1661d9a31a8
johnfrankmorgan <johnfrankmorgan@gmail.com>
2021-09-12 09:46:16 +0100

    Add string to CompanyNumber conversion function

--- company_number.go
+++ company_number.go
@@ -1,33 +1,52 @@
 package comphouse
 
-import "fmt"
-
-type CompanyNumber interface {
-	fmt.Stringer
-	Next() CompanyNumber
-}
-
-var (
-	_ CompanyNumber = EnglishCompanyNo(0)
-	_ CompanyNumber = ScottishCompanyNo(0)
+import (
+	"fmt"
+	"strconv"
+	"strings"
 )
 
+// CompanyNumber is an interface to convert a value into a valid company number
+type CompanyNumber interface {
+	fmt.Stringer
+}
+
+// CompanyNumberFromString creates a new CompanyNumber from the provided string
+// It returns an error if the input cannot be converted to a CompanyNumber
+func CompanyNumberFromString(s string) (CompanyNumber, error) {
+	s = strings.ToUpper(s)
+
+	f := func(num uint64) CompanyNumber {
+		return EnglishCompanyNo(num)
+	}
+
+	if strings.HasPrefix(s, "SC") {
+		f = func(num uint64) CompanyNumber {
+			return ScottishCompanyNo(num)
+		}
+		s = s[2:]
+	}
+
+	num, err := strconv.ParseUint(s, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	return f(num), nil
+}
+
+// EnglishCompanyNo is a CompanyNumber implementation for English companies
 type EnglishCompanyNo uint
 
+// String satisfies the CompanyNumber interface
 func (m EnglishCompanyNo) String() string {
 	return fmt.Sprintf("%08d", m)
 }
 
-func (m EnglishCompanyNo) Next() CompanyNumber {
-	return m + 1
-}
-
+// ScottishCompanyNo is a CompanyNumber implementation for Scottish companies
 type ScottishCompanyNo uint
 
+// String satisfies the CompanyNumber interface
 func (m ScottishCompanyNo) String() string {
 	return fmt.Sprintf("SC%06d", m)
-}
-
-func (m ScottishCompanyNo) Next() CompanyNumber {
-	return m + 1
 }
--- company_number_test.go
+++ company_number_test.go
@@ -12,7 +12,6 @@ func TestEnglishCompanyNo(t *testing.T) {
 	no := EnglishCompanyNo(100100)
 
 	assert.Equal("00100100", no.String())
-	assert.Equal("00100101", no.Next().String())
 }
 
 func TestScottishCompanyNo(t *testing.T) {
@@ -21,5 +20,40 @@ func TestScottishCompanyNo(t *testing.T) {
 	no := ScottishCompanyNo(100100)
 
 	assert.Equal("SC100100", no.String())
-	assert.Equal("SC100101", no.Next().String())
+}
+
+func TestCompanyNumberFromString(t *testing.T) {
+	type test struct {
+		inp string
+		exp CompanyNumber
+	}
+
+	tests := []test{
+		{"SC000104", ScottishCompanyNo(104)},
+		{"10010010", EnglishCompanyNo(10010010)},
+		{"SC123123", ScottishCompanyNo(123123)},
+		{"1", EnglishCompanyNo(1)},
+	}
+
+	for _, test := range tests {
+		t.Run(test.inp, func(t *testing.T) {
+			assert := assert.New(t)
+
+			number, err := CompanyNumberFromString(test.inp)
+
+			if assert.NoError(err) {
+				assert.Equal(test.exp, number)
+			}
+		})
+	}
+
+	t.Run("handles errors", func(t *testing.T) {
+		assert := assert.New(t)
+
+		number, err := CompanyNumberFromString("SC")
+		if assert.Error(err) {
+			assert.Nil(number)
+			assert.Contains(err.Error(), "invalid syntax")
+		}
+	})
 }
